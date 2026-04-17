9f64ca37c834b0db2a3e200083185fdc674ebb6d
HassanBahati <mukisabahati@gmail.com>
2026-04-17 19:30:37 +0300

    chore: add pssu.ug to allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-replace-this-with-a-secure-value"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.pssu.ug", "pssu.ug"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
