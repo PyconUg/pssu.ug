c49cc968d98fb875e2cf03f72926d1ae6f891fcf
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2026-04-27 10:45:03 +0300

    regional pages questionair

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from website import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
+    path("apply/<str:country>/", views.apply, name="apply"),
+    path("apply/<str:country>/success/", views.apply_success, name="apply_success"),
 ]
